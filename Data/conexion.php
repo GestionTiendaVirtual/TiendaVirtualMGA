44cc566f3fabf361af219cc52ad512158e427e9f
GustavoNajera <tavinchi.com@gmail.com>
2017-01-21 07:21:58 -0600

    Eliminacion de adornos en interfaz CRUD Cuenta

--- Data/conexion.php
+++ Data/conexion.php
@@ -1,6 +1,7 @@
 <?php
 	
-	$mysqli = new mysqli("localhost","root","","gestion"); //servidor, usuario de base de datos, contraseña del usuario, nombre de base de datos
+	$mysqli = new mysqli("localhost","root","","mgasolucionesdb"); //servidor, usuario de base de datos, contraseña del usuario, nombre de base de datos
+
 	
 	if(mysqli_connect_errno()){
 		echo 'Conexion Fallida : ', mysqli_connect_error();
