c4266dbeeb54597aeea2e88829fef38898862303
Anthony <tony950819@gmail.com>
2017-01-23 01:48:48 -0600

    Muro

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
     	<a href="./Presentation/ClientAccountRetrieve.php">CRUD cuenta</a>
         <a href="./Presentation/ProductCreate.php">CRUD Producto</a>
         <a href="./Presentation/combo.php">Dirección cliente</a>
+        <a href="./Presentation/productOption.php">Muro de productos</a>
         <hr>
             <?php
             
